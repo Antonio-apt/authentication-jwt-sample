4c232bbdecd2bf1643cc46ec6677f92ec8b4d9f5
Antonio Souza <antonios16020@gmail.com>
2020-04-24 14:44:24 -0300

    add eslint and start a migration

---
+++ database/migrations/20200423205650-create-users.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncremente: true,
+        primaryKey: true,
+        type: Sequelize.INTENGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => queryInterface.dropTable("Users"),
+};
